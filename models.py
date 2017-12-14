4d0611705c37f3d24b550d9c03d0cf8ce8eaa0ff
Abhishek Choudhary <acabhishek942@gmail.com>
2017-12-14 10:50:45 +0530

    update models

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    unique_id = db.Column(db.String, primary_key=True)
+    unique_id = db.Column(db.String, primary_key=True, unique=True)
     email = db.Column(db.String(120), unique=True, nullable=False, primary_key=True)
     campaigns = db.relationship('Campaign', backref = 'user', lazy = True)
 
@@ -12,16 +12,17 @@ class User(db.Model):
 
 
 class Campaign(db.Model):
-    unique_id = db.Column(db.String, primary_key=True)
+    unique_id = db.Column(db.String, primary_key=True, unique=True)
     title  = db.Column(db.String(120), unique=True, nullable=False)
     stages = db.Column(db.Integer, nullable = False)
     frequency =  db.Column(db.Integer, nullable = False) #needs-attention
     recipients =  db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.unique_id'), nullable=False)
+    email = db.relationship('Email', backref = 'campaign', lazy = True)
+    user_id = db.Column(db.String, db.ForeignKey('user.unique_id'), nullable=False)
 
 
 class Email(db.Model):
-    unique_id = db.Column(db.String, primary_key=True)
+    unique_id = db.Column(db.String, primary_key=True, unique=True)
     subject =  db.Column(db.String(200), nullable = False)
     body  = db.Column(db.Text, nullable =  False)
-    campaign_id = db.Column(db.Integer, db.ForeignKey('campaign.unique_id'),  nullable=False)
+    campaign_id = db.Column(db.String, db.ForeignKey('campaign.unique_id'),  nullable=False)
